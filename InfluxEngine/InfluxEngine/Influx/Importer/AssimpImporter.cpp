4963b6b945e53b46c509d394e054b4b0459c04c2
Kairoz <52052044+Kair0z@users.noreply.github.com>
2021-01-21 14:01:55 +0100

    AssimpImporter Initial Setup

---
+++ InfluxEngine/InfluxEngine/Influx/Importer/AssimpImporter.cpp
@@ -0,0 +1,10 @@
+#include "AssimpImporter.h"
+
+bool AssimpImporter::Import(const std::string& file)
+{
+	std::string fullpath;
+	// TODO: Manage finding files by name
+
+	
+	return false;
+}
---
+++ InfluxEngine/InfluxEngine/Influx/Importer/AssimpImporter.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+namespace Influx
+{
+	class AssimpImporter final
+	{
+	public:
+		static bool Import(const std::string& file);
+
+	private:
+
+	};
+}
+
+
