47b6337c12834875b58292df4e899ed375bb47a8
Kairoz <52052044+Kair0z@users.noreply.github.com>
2021-01-25 15:19:59 +0100

    Added Resource Loading classes

---
+++ InfluxEngine/InfluxEngine/Influx/Resources/ResourceManagement/ILoader.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <unordered_map>
+#include "Loading.h"
+
+namespace Influx
+{
+	namespace Intern
+	{
+		class ILoader
+		{
+		public:
+			virtual ~ILoader() = default;
+			virtual const type_info& GetType() const = 0;
+		};
+	}
+
+	template <class Resource>
+	class Loader : public ILoader
+	{
+	public:
+		~Loader();
+		virtual Resource* GetResource(const std::string& fName);
+		virtual const type_info& GetType() const override final;
+		virtual Resource* LoadResource(const std::string& fName);
+
+	protected:
+		virtual void Unload(Resource* pObj);
+		std::unordered_map<std::string, Resource*> mContentMap;
+	};
+
+#pragma region Impl
+	template<class Resource>
+	inline Loader<Resource>::~Loader()
+	{
+		for (auto& pair : mContentMap)
+			Destroy(pair.second);
+	}
+
+	template<class Resource>
+	inline const type_info& Loader<Resource>::GetType() const
+	{
+		return typeid(Resource);
+	}
+
+	template<class Resource>
+	inline Resource* Loader<Resource>::LoadResource(const std::string& fName)
+	{
+		return Loading::LoadContent<Resource>(fName);
+	}
+
+	template<class Resource>
+	inline void Loader<Resource>::Unload(Resource* pObj)
+	{
+		Loading::UnloadResource<Resource>(pObj);
+	}
+
+	template<class Resource>
+	inline Resource* Loader<Resource>::GetResource(const std::string& fName)
+	{
+		// If the content doesn't yet exists load & return it...
+		if (!mContentMap[fName]) 
+			mContentMap[fName] = LoadContent(fName);
+
+		return mContentMap[fName];
+	}
+#pragma endregion
+}
+
+
